bb2d87ef4b32aa4c168ce05d262afa37d1e56770
Sharik Mohammed <Sharik_Mohammed@epam.com>
2020-02-26 18:11:56 +0530

    updated all the other tasks

---
+++ HomeTask-2/JSTask-1/transportationOnVacation.js
@@ -0,0 +1,21 @@
+/*
+Problem link: http://www.codewars.com/kata/transportation-on-vacation
+*/
+
+function rentalCarCost(d) {
+  var everyDayCost = 40;
+  var discount;
+
+  var totalAmount = d * everyDayCost;
+
+  if(d >= 7) {
+    totalAmount = totalAmount - 50;
+    return totalAmount;
+  }
+
+  else if(d >= 3) {
+    totalAmount = totalAmount - 20;
+    return totalAmount;
+  }
+  return totalAmount;
+}
---
+++ HomeTask-2/JSTask-2/getTheMiddleCharacter.js
@@ -0,0 +1,20 @@
+/*
+Problem link: http://www.codewars.com/kata/get-the-middle-character
+*/
+
+function getMiddle(s) {
+  //Code goes here!
+  var len = s.length;
+  var mid = len / 2;
+
+  if (len == 1) {
+    return s;
+  }
+
+  if (len % 2 == 0) {
+
+    return s.substring(Math.trunc(mid) - 1, Math.trunc(mid) + 1);
+  }
+
+  return s.substring(Math.trunc(mid), Math.trunc(mid) + 1);
+}
