5f7718a4bd5c8092122285ec9580d4b3a2a36492
Sharikzama <sidsharik@yahoo.com>
2020-04-08 15:58:37 +0530

    Update load-page.js

--- HomeTask-3-Youtube-API/load-page.js
+++ HomeTask-3-Youtube-API/load-page.js
@@ -1,15 +1,15 @@
 function init() {
-  var formDivId = document.createElement("div");
+  const formDivId = document.createElement("div");
   formDivId.setAttribute('id', 'search-form');
-  var label = document.createElement("div");
+  const label = document.createElement("div");
   label.setAttribute('id', 'label');
   label.innerHTML = "YouTube Query Search";
   formDivId.appendChild(label);
 
-  var lineBreak = document.createElement("br");
+  const lineBreak = document.createElement("br");
   formDivId.appendChild(lineBreak);
 
-  var inputType = document.createElement("input");
+  const inputType = document.createElement("input");
   inputType.setAttribute("type", "text");
   inputType.setAttribute("id", "incomingQuery");
   inputType.setAttribute("name", "inputQuery");
@@ -17,36 +17,27 @@ function init() {
   inputType.setAttribute("size", 50);
   formDivId.appendChild(inputType);
 
-  var button = document.createElement("button");
+  const button = document.createElement("button");
   button.setAttribute("id", "querySearch");
   button.setAttribute('onclick', 'searchByKeyword();');
   button.innerHTML = "Search";
   formDivId.appendChild(button);
 
-  var nextButton = document.createElement("button");
+  const nextButton = document.createElement("button");
   nextButton.setAttribute("id", "buttonNext");
   nextButton.innerHTML = "Next Page";
   nextButton.setAttribute("onclick", "nextPage()");
   formDivId.appendChild(nextButton);
 
-  var lineBreak = document.createElement("br");
+  const lineBreak = document.createElement("br");
   formDivId.appendChild(lineBreak);
 
-  var prevButton = document.createElement("button");
+  const prevButton = document.createElement("button");
   prevButton.setAttribute("id", "buttonPrev");
   prevButton.innerHTML = "Previous Page";
   prevButton.setAttribute("onclick", "prevPage()");
   formDivId.appendChild(prevButton);
-
-  // var label = document.createElement("label");
-  // label.setAttribute("name:","current page number");
-  // var currentPageNumber = document.createElement("input");
-  // currentPageNumber.setAttribute("type", "text");
-  // currentPageNumber.setAttribute("id", "currentPageNumber");
-  // currentPageNumber.setAttribute("name", "currentPageNumber");
-  // currentPageNumber.setAttribute("placeholder", "Page number");
-  // currentPageNumber.setAttribute("size", 20);
-  // formDivId.appendChild(currentPageNumber);
+  
   document.querySelector('body').prepend(formDivId);
 
 }
